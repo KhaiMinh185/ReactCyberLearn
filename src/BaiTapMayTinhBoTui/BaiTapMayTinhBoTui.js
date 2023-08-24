d3bae66bae5e46c1e0a9f0d5b0d8e501722fe29b
KhaiMinh185 <dangkhaiminh18052002@gmail.com>
2023-08-24 19:02:23 +0700

    cal

--- src/BaiTapMayTinhBoTui/BaiTapMayTinhBoTui.js
+++ src/BaiTapMayTinhBoTui/BaiTapMayTinhBoTui.js
@@ -3,72 +3,34 @@ import React, { Component } from 'react';
 export default class BaiTapMayTinhBoTui extends Component {
 
   state = {
-    num1: '',
-    num2: '',
-    operator: '',
+    result: "",
   };
 
-  dis = (val) => {
-    let resultInput = document.querySelector('#screen');
-    console.log(val);
+  dis = (e) => {
+    // let number = parseInt(e.target.innerText, 10);
+    let number = e.target.innerText;
 
-    if (isNaN(val)) {
-      if (val === 'C') {
-        this.setState({
-          num1: '',
-          num2: '',
-          operator: 'undefined',
-        });
-        resultInput.value = '';
-        return;
-      }
-      if (val === '=') {
-        const result = this.perform(this.state.num1, this.state.num2, this.state.operator);
-        // resultInput.value = '';
-        resultInput.value = result;
-        console.log(result);
-        this.setState({
-          num1: '',
-          num2: '',
-          operator: 'undefined',
-        });
-      } else {
-        this.setState({
-          operator: val,
-        });
-      }
-    } else {
-      if (this.state.operator) {
-        this.setState({
-          num1: resultInput.value += val,
-        });
-      } else {
-        this.setState({
-          num2: resultInput.value += val,
-        });
-      }
-    }
-  }
+    this.setState({
+      result: this.state.result + number,
+    });
 
-  perform = (num1, num2, operator) => {
-    num1 = Number(num1);
-    num2 = Number(num2);
+  };
 
-    switch (operator) {
-      case '+':
-        return num1 + num2;
-      case '-':
-        return num1 - num2;
-      case '*':
-        return num1 * num2;
-      case '/':
-        if (num2 === 0) {
-          return "Division by zero";
-        } else {
-          return num1 / num2;
-        }
-      default:
-        return;
+  clear = () => {
+    this.setState({
+      result: "",
+    });
+  };
+
+  calculator = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result.toString()),
+      });
+    } catch (error) {
+      this.setState({
+        result: "error",
+      });
     }
   };
 
@@ -82,35 +44,35 @@ export default class BaiTapMayTinhBoTui extends Component {
       <div className='container-fluid' style={{ width: '40%' }}>
         <div className="card text-left" style={{ backgroundColor: 'rgba(0,0,0,.03)' }}>
           <h5 className='card-header'>
-            <input id='screen' className='text-right w-100'></input>
+            <input id='screen' className='text-right w-100' value={this.state.result}></input>
           </h5>
           <table className="table text-center m-0">
             <tbody>
               <tr>
-                <td value='7' onClick={() => { this.dis('7') }} style={mystyle}>7</td>
-                <td value='8' onClick={() => { this.dis('8') }} style={mystyle}>8</td>
-                <td value='9' onClick={() => { this.dis('9') }} style={mystyle}>9</td>
-                <td value='/' onClick={() => { this.dis('/') }} style={mystyle}>/</td>
-                <td value='CE' onClick={() => { this.dis('CE') }} style={mystyle}>CE</td>
+                <td name="7" onClick={this.dis} style={mystyle}>7</td>
+                <td name="8" onClick={this.dis} style={mystyle}>8</td>
+                <td name="9" onClick={this.dis} style={mystyle}>9</td>
+                <td name="/" onClick={this.dis} style={mystyle}>&divide;</td>
+                <td name="C" onClick={this.dis} style={mystyle}>CE</td>
               </tr>
               <tr>
-                <td value='4' onClick={() => { this.dis('4') }} style={mystyle}>4</td>
-                <td value='5' onClick={() => { this.dis('5') }} style={mystyle}>5</td>
-                <td value='6' onClick={() => { this.dis('6') }} style={mystyle}>6</td>
-                <td value='*' onClick={() => { this.dis('*') }} style={mystyle}>*</td>
-                <td value='C' onClick={() => { this.dis('C') }} style={mystyle}>C</td>
+                <td name="4" onClick={this.dis} style={mystyle}>4</td>
+                <td name="5" onClick={this.dis} style={mystyle}>5</td>
+                <td name="6" onClick={this.dis} style={mystyle}>6</td>
+                <td name="*" onClick={this.dis} style={mystyle}>&times;</td>
+                <td name="C" onClick={this.clear} style={mystyle}>C</td>
               </tr>
               <tr>
-                <td value='1' onClick={() => { this.dis('1') }} style={mystyle}>1</td>
-                <td value='2' onClick={() => { this.dis('2') }} style={mystyle}>2</td>
-                <td value='3' onClick={() => { this.dis('3') }} style={mystyle}>3</td>
-                <td value='-' onClick={() => { this.dis('-') }} style={mystyle}>-</td>
-                <td value='=' onClick={() => { this.dis('=') }} style={mystyle} rowSpan={2}>=</td>
+                <td name="1" onClick={this.dis} style={mystyle}>1</td>
+                <td name="2" onClick={this.dis} style={mystyle}>2</td>
+                <td name="3" onClick={this.dis} style={mystyle}>3</td>
+                <td name="-" onClick={this.dis} style={mystyle}>&ndash;</td>
+                <td name="=" onClick={this.calculator} style={mystyle} rowSpan={2}>=</td>
               </tr>
               <tr>
-                <td value='0' onClick={() => { this.dis('0') }} style={mystyle} colSpan={2}>0</td>
-                <td value='1' onClick={() => { this.dis('1') }} style={mystyle}>1</td>
-                <td value='+' onClick={() => { this.dis('+') }} style={mystyle}>+</td>
+                <td name="0" onClick={this.dis} style={mystyle} colSpan={2}>0</td>
+                <td name="1" onClick={this.dis} style={mystyle}>1</td>
+                <td name="+" onClick={this.dis} style={mystyle}>+</td>
               </tr>
             </tbody>
           </table>
